945c9b68bd0b59d1dccf761bf8260f367d4eab74
areeb111 <maxima111>
2023-04-12 01:43:59 +0300

    Adding StringLibrary

---
+++ ShowCase/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace UtilityLibraries;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        String test = "Hello world";
+        Boolean check = test.StartsWithUpper();
+        Console.WriteLine("Result : " + check);
+        Console.ReadKey();
+
+    }
+}
+
+
+
+
+
+
